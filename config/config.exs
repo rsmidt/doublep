39ac7c7c832327496aa4acf86512da8894ce9f86
Ruben Smidt <ruben@rsmidt.dev>
2023-02-08 21:11:05 +0100

    fix: add basic otlp config

--- config/config.exs
+++ config/config.exs
@@ -51,6 +51,13 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+config :opentelemetry,
+  span_processor: :batch,
+  traces_exporter: :otlp
+
+config :opentelemetry_exporter,
+  otlp_protocol: :http_protobuf
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
