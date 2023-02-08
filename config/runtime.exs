cecf52b36b5b9a325559bbd5548c710c336ffb58
Ruben Smidt <ruben@rsmidt.dev>
2023-02-08 19:17:13 +0100

    chore: rename runtime environment variables

--- config/runtime.exs
+++ config/runtime.exs
@@ -22,7 +22,7 @@ end
 
 if config_env() == :prod do
   database_url =
-    System.get_env("DATABASE_URL") ||
+    System.get_env("DOUBLEP_DATABASE_URL") ||
       raise """
       environment variable DATABASE_URL is missing.
       For example: ecto://USER:PASS@HOST/DATABASE
@@ -33,7 +33,7 @@ if config_env() == :prod do
   config :doublep, Doublep.Repo,
     # ssl: true,
     url: database_url,
-    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+    pool_size: String.to_integer(System.get_env("DOUBLEP_ECTO_POOL_SIZE") || "10"),
     socket_options: maybe_ipv6
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
@@ -42,14 +42,14 @@ if config_env() == :prod do
   # to check this value into version control, so we use an environment
   # variable instead.
   secret_key_base =
-    System.get_env("SECRET_KEY_BASE") ||
+    System.get_env("DOUBLEP_PHX_SECRET_KEY_BASE") ||
       raise """
       environment variable SECRET_KEY_BASE is missing.
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "example.com"
-  port = String.to_integer(System.get_env("PORT") || "4000")
+  host = System.get_env("DOUBLEP_PHX_HOST") || "example.com"
+  port = String.to_integer(System.get_env("DOUBLEP_PHX_PORT") || "4000")
 
   config :doublep, DoublepWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
